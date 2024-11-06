322634c93577bf7a953a898f5d5d0f76bd78e1ba
Fahar Ahnaf Azis <faharahnaf66@gmail.com>
2024-11-06 23:35:59 +0700

    fix: get data in session local storage

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -13,12 +13,21 @@ import {
   faWallet,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AnimateHeight from "react-animate-height";
 
 const UserProfile = () => {
   const [currentMenu, setCurrentMenu] = useState<string>("");
+  const [data, setData] = useState<any>("");
+  const [onChange, setOnChange] = useState<any>(null);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("userSession");
+    console.log("Stored Data:", storedData);
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    }
+  }, []);
 
   const toggleMenu = (value: string) => {
     setCurrentMenu((oldValue) => {
@@ -26,36 +35,32 @@ const UserProfile = () => {
     });
   };
 
-  // const { t } = getTranslation();
   return (
-    <div className="">
-      <Navbar></Navbar>
-      <div className="w-[35%] min-h-screen m-10 mt-20">
+    <div className="flex justify-center">
+      <Navbar />
+      <div className="m-10 my-28 w-96 p-5 shadow-xl rounded-xl">
         <div className="flex gap-4 border-b-2 py-5">
-          <div className="bg-slate-400 rounded-full size-24"></div>
+          <div className="bg-slate-400 rounded-full size-24" />
           <div className="flex-col flex justify-center">
-            <p>Your name</p>
-            <p>yourname@gmail.com</p>
+            <p>{data.name || "loading..."} </p>
+            <p>{data.email || "loading..."}</p>
           </div>
         </div>
+
         <div className="space-y-20">
           <ul className="mx-2 my-8 space-y-6">
             {/* Button Profile */}
-            <li className="flex flex-col hover:text-blue-300">
+            <li className="flex flex-col">
               <button
                 type="button"
                 className={`${currentMenu === "profile" ? "active" : ""}`}
                 onClick={() => toggleMenu("profile")}
               >
-                <div className="flex items-center gap-3 hover:text-blue-300">
+                <div className="flex items-center gap-3">
                   <FontAwesomeIcon icon={faUser} className="w-5" />
                   <p>Profile</p>
-                  <div
-                    className={`${
-                      currentMenu == "profile" ? "rotate-90 rtl:rotate-90" : ""
-                    } ml-auto`}
-                  >
-                    <FontAwesomeIcon icon={faAngleRight} className="" />
+                  <div className="ml-auto">
+                    <FontAwesomeIcon icon={faAngleRight} />
                   </div>
                 </div>
               </button>
@@ -71,11 +76,11 @@ const UserProfile = () => {
                   <FontAwesomeIcon icon={faGear} className="w-5" />
                   <span className="text-black">Settings</span>
                   <div
-                    className={`${
-                      currentMenu == "settings" ? "rotate-90 rtl:rotate-90" : ""
-                    } ml-auto`}
+                    className={`ml-auto ${
+                      currentMenu == "settings" ? "rotate-90" : ""
+                    }`}
                   >
-                    <FontAwesomeIcon icon={faAngleRight} className="" />
+                    <FontAwesomeIcon icon={faAngleRight} />
                   </div>
                 </div>
               </button>
@@ -125,8 +130,8 @@ const UserProfile = () => {
                       </div>
                     </button>
                   </li>
-                  <li className=" hover:bg-blue-300">
-                    <button className="flex items-center gap-3 w-full ">
+                  <li className="hover:bg-blue-300">
+                    <button className="flex items-center gap-3 w-full">
                       <FontAwesomeIcon icon={faBell} className="w-5" />
                       <div className="text-left">
                         <p>Notifikasi</p>
@@ -156,35 +161,60 @@ const UserProfile = () => {
               <p>Logout</p>
             </li>
           </ul>
-
-          {/* <ul className="mx-2 my-8 space-y-6">
-            <li className="flex items-center gap-3">
-              <FontAwesomeIcon icon={faUser} />
-              <p>My Profile</p>
-              <FontAwesomeIcon icon={faAngleRight} className="ml-auto" />
-            </li>
-            <li className="flex items-center gap-3">
-              <FontAwesomeIcon icon={faUser} />
-              <p>My Profile</p>
-              <FontAwesomeIcon icon={faAngleRight} className="ml-auto" />
-            </li>
-            <li className="flex items-center gap-3">
-              <FontAwesomeIcon icon={faUser} />
-              <p>My Profile</p>
-              <FontAwesomeIcon icon={faAngleRight} className="ml-auto" />
-            </li>
-            <li className="flex items-center gap-3">
-              <FontAwesomeIcon icon={faUser} />
-              <p>My Profile</p>
-              <FontAwesomeIcon icon={faAngleRight} className="ml-auto" />
-            </li>
-            <li className="flex items-center gap-3">
-              <FontAwesomeIcon icon={faUser} />
-              <p>My Profile</p>
-              <FontAwesomeIcon icon={faAngleRight} className="ml-auto" />
-            </li>
-          </ul> */}
         </div>
+      </div>
+
+      <div className=" m-10 my-28 w-[50%] p-5 shadow-xl rounded-xl ">
+        <ul className="mx-2 my-8 space-y-6">
+          <li className="flex items-center gap-3 border-b-2 py-5">
+            <FontAwesomeIcon icon={faUser} />
+            <p>My Profile</p>
+            <FontAwesomeIcon icon={faAngleRight} className="ml-auto" />
+          </li>
+          <form action="">
+            <li className="flex items-center gap-3 py-5 border-b-2 justify-between">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className=" w-1/2 h-10 text-right"
+                defaultValue={data.name}
+                onChange={(e) => setOnChange(e.target.value)}
+              />
+            </li>
+            <li className="flex items-center gap-3 py-5 border-b-2 justify-between">
+              <label htmlFor="name">Email Account</label>
+              <input
+                type="text"
+                className=" w-1/2 h-10 text-right"
+                defaultValue={data.email}
+                // onChange={(e) => setOnChange(e.target.value)}
+              />
+            </li>
+            <li className="flex items-center gap-3 py-5 border-b-2 justify-between">
+              <label htmlFor="name">Mobile Number</label>
+              <input
+                type="text"
+                className=" w-1/2 h-10 text-right"
+                defaultValue={"your phone number"}
+                // onChange={(e) => setOnChange(e.target.value)}
+              />
+            </li>
+            <li className="flex items-center gap-3 py-5 border-b-2 justify-between">
+              <label htmlFor="name">Location</label>
+              <input
+                type="text"
+                className=" w-1/2 h-10 text-right"
+                defaultValue={"your Location"}
+                // onChange={(e) => setOnChange(e.target.value)}
+              />
+            </li>
+            <li className="flex items-center gap-3 py-5  justify-between">
+              <button className="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-lg">
+                Save Change
+              </button>
+            </li>
+          </form>
+        </ul>
       </div>
     </div>
   );
